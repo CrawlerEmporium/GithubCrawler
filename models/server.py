b7c7d071d57a7a946876bfee12481d80c44e1360
LordDusk <lang2619@gmail.com>
2020-07-26 12:24:38 +0200

    feat(issue): Updated model

--- models/server.py
+++ models/server.py
@@ -1,17 +1,18 @@
 class Listen:
-    def __init__(self, channel: int, tracker: int, identifier: str, repo: str):
+    def __init__(self, channel: int, tracker: int, identifier: str, type: str, repo: str):
         self.channel = channel
         self.tracker = tracker
         self.identifier = identifier
+        self.type = type
         self.repo = repo
 
     @classmethod
     def from_data(cls, data):
-        return cls(data['channel'], data['tracker'], data['identifier'], data.get('repo', None))
+        return cls(data['channel'], data['tracker'], data['identifier'], data['type'], data.get('repo', None))
 
     def to_dict(self):
         return {
-            'channel': self.channel, 'tracker': self.tracker, 'identifier': self.identifier, 'repo': self.repo
+            'channel': self.channel, 'tracker': self.tracker, 'identifier': self.identifier, 'type': self.type, 'repo': self.repo
         }
 
 
