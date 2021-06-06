58d5e9045deddf11ece9a111c2f6329507cc5087
LordDusk <lang2619@gmail.com>
2021-06-06 15:34:15 +0200

    feat(owner): in-discord update added

--- cogs/owner.py
+++ cogs/owner.py
@@ -1,16 +1,40 @@
 import os
-from discord.ext import commands
+import subprocess
+import inspect
 
 import utils.globals as GG
+from discord.ext import commands
+
 from utils import logger
 
 log = logger.logger
 
+
 extensions = [x.replace('.py', '') for x in os.listdir(GG.COGS) if x.endswith('.py')]
+path = GG.COGS + '.'
 
 class Owner(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+
+    @commands.command(hidden=True)
+    @commands.is_owner()
+    async def gitpull(self, ctx):
+        """Pulls from github and updates bot"""
+        await ctx.trigger_typing()
+        await ctx.send(f"```{subprocess.run('git pull', stdout=subprocess.PIPE, shell=True).stdout.decode('utf-8')}```")
+        for cog in extensions:
+            ctx.bot.unload_extension(f'{path}{cog}')
+        for cog in extensions:
+            members = inspect.getmembers(cog)
+            for name, member in members:
+                if name.startswith('on_'):
+                    ctx.bot.add_listener(member, name)
+            try:
+                ctx.bot.load_extension(f'{path}{cog}')
+            except Exception as e:
+                await ctx.send(f'LoadError: {cog}\n{type(e).__name__}: {e}')
+        await ctx.send('All cogs reloaded :white_check_mark:')
 
     @commands.command(hidden=True)
     @commands.is_owner()
