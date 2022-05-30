5de6101c4d923136c3ec0cab9712f4549b12efb9
LordDusk <lang2619@gmail.com>
2022-05-30 23:52:29 +0200

    Maybe fix the len() error

--- utils/autocomplete.py
+++ utils/autocomplete.py
@@ -1,6 +1,7 @@
 import discord
 from utils import globals as GG
 
+
 async def get_server_feature_identifiers(ctx: discord.AutocompleteContext):
     server = await GG.MDB.Github.find_one({"server": ctx.interaction.guild_id})
     identifiers = []
@@ -36,7 +37,13 @@ async def get_server_reports(ctx: discord.AutocompleteContext):
         GG.cachedTrackerChannels.append({"guild_id": ctx.interaction.guild_id, "channels": trackerChannels})
     cachedTrackers = next(server["channels"] for server in GG.cachedTrackerChannels if server['guild_id'] == ctx.interaction.guild_id)
     reports = await GG.MDB.Reports.find({"trackerId": {"$in": cachedTrackers}}).to_list(length=None)
-    if reports is not None:
-        return [f"{report['report_id']} | {report['title'][:85]+'...' if report is not None and len(report['title']) >= 90 else report['title']}" for report in reports if ctx.value.upper() in report['report_id']]
-    return []
+    stringReports = []
+    for report in reports:
+        if len(report['title']) > 90:
+            stringReports.append({"id": report['report_id'], "title": f"{report['report_id']} | {report['title'][:85]}...".upper()})
+        else:
+            stringReports.append({"id": report['report_id'], "title": f"{report['report_id']} | {report['title']}".upper()})
 
+    if stringReports is not None:
+        return [f"{report['id']}" for report in reports if ctx.value.upper() in report['title']]
+    return []
