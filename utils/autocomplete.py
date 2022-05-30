b2ad4faa37c735cbedba3dd7d742420dea1c0339
LordDusk <lang2619@gmail.com>
2022-05-31 00:02:34 +0200

    revert as it didn't work sadly.

--- utils/autocomplete.py
+++ utils/autocomplete.py
@@ -1,7 +1,6 @@
 import discord
 from utils import globals as GG
 
-
 async def get_server_feature_identifiers(ctx: discord.AutocompleteContext):
     server = await GG.MDB.Github.find_one({"server": ctx.interaction.guild_id})
     identifiers = []
@@ -37,10 +36,7 @@ async def get_server_reports(ctx: discord.AutocompleteContext):
         GG.cachedTrackerChannels.append({"guild_id": ctx.interaction.guild_id, "channels": trackerChannels})
     cachedTrackers = next(server["channels"] for server in GG.cachedTrackerChannels if server['guild_id'] == ctx.interaction.guild_id)
     reports = await GG.MDB.Reports.find({"trackerId": {"$in": cachedTrackers}}).to_list(length=None)
-    stringReports = []
-    for report in reports:
-        stringReports.append({"id": report['report_id'], "title": f"{report['report_id']} | {report['title'][:85]}"})
-
-    if stringReports is not None:
-        return [f"{report['id']}" for report in reports if ctx.value.upper() in report['title'].upper()]
+    if reports is not None:
+        return [f"{report['report_id']} | {report['title'][:85]+'...' if report is not None and len(report['title']) >= 90 else report['title']}" for report in reports if ctx.value.upper() in report['report_id']]
     return []
+
