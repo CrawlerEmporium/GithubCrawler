9e6ae7704163521a79334dc3c5db4df3ac17faf8
Lorddusk <lang2619@gmail.com>
2022-08-17 09:56:50 +0200

    Manager commands slashified.

--- cogs/manager.py
+++ cogs/manager.py
@@ -1,6 +1,7 @@
 import typing
 
 import discord
+from discord import SlashCommandGroup, Option
 from discord.ext import commands
 
 from utils import globals as GG
@@ -13,52 +14,39 @@ class Manager(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.group(invoke_without_command=True)
-    @commands.guild_only()
-    async def manager(self, ctx):
-        prefix = await self.bot.get_server_prefix(ctx.message)
-        await ctx.send("**Valid options currently are:**\n"
-                       f"```{prefix}manager\n"
-                       f"{prefix}manager add [member]\n"
-                       f"{prefix}manager remove [member]\n"
-                       f"{prefix}manager list```")
+    manager = SlashCommandGroup("manager", "All commands that have effect on managers for your server",
+                              checks=[commands.guild_only().predicate])
 
     @manager.command(name='add')
-    @commands.guild_only()
-    @commands.has_permissions(administrator=True)
-    async def managerAdd(self, ctx, member: typing.Optional[discord.Member]):
-        if member is None:
-            await ctx.send("Invalid member, please check the name/id and try again.")
-            return
-
+    @discord.default_permissions(
+        administrator=True,
+    )
+    async def managerAdd(self, ctx, member: Option(discord.Member, description="Who do you want to add as a manager?")):
         manager = await GG.MDB.Managers.find_one({"user": member.id, "server": ctx.guild.id})
         if manager is not None:
-            await ctx.send("Manager already found in the database.")
+            await ctx.respond("Manager already found in the database.")
             return
 
         await GG.MDB.Managers.insert_one({"user": member.id, "server": ctx.guild.id})
-        await ctx.send(f"{member.mention} was added as an issue manager.")
+        await ctx.respond(f"{member.mention} was added as an issue manager.")
 
     @manager.command(name='remove')
-    @commands.guild_only()
-    @commands.has_permissions(administrator=True)
-    async def managerRemove(self, ctx, member: typing.Optional[discord.Member]):
-        if member is None:
-            await ctx.send("Invalid member, please check the name/id and try again.")
-
+    @discord.default_permissions(
+        administrator=True,
+    )
+    async def managerRemove(self, ctx, member: Option(discord.Member, description="Who do you want to remove as a manager?")):
         manager = await GG.MDB.Managers.find_one({"user": member.id, "server": ctx.guild.id})
         if manager is None:
-            await ctx.send("Manager not found in the database, please check the name/id and try again.")
+            await ctx.respond("Manager not found in the database, please check the name/id and try again.")
 
         await GG.MDB.Managers.delete_one({"user": member.id, "server": ctx.guild.id})
-        await ctx.send(f"{member.mention} was removed as an issue manager.")
+        await ctx.respond(f"{member.mention} was removed as an issue manager.")
 
     @manager.command(name='list')
-    @commands.guild_only()
     async def managerList(self, ctx):
         server = await GG.MDB.Managers.find({"server": ctx.guild.id}).to_list(length=None)
         if server is None or len(server) <= 0:
-            await ctx.send("This server has no managers (Except the Server Owner).")
+            await ctx.respond("This server has no managers (Except the Server Owner).")
         else:
             channels = "This server has the following managers:\n\n"
             for x in server:
@@ -67,7 +55,7 @@ class Manager(commands.Cog):
                     channels += f"{x['user']}\n"
                 else:
                     channels += f"{user.mention}\n"
-            await ctx.send(channels)
+            await ctx.respond(channels)
 
 
 def setup(bot):
